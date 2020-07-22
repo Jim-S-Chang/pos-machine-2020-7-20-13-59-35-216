d34ea28a2250db93fc398381ad6b67c2a6207936
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-22 11:16:02 +0800

    [jim]bug-fix:change some code style given in code review.

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function printReceipt(barcodes) {
-    let itemInfos = getUnitPriceAndName(barcodes)
-    let itemList = integrateSameItems(itemInfos)
-    let receiptItems = calculateSubtotalPrice(itemList)
-    let totalPrice = calculateTotalPrice(receiptItems)
-    console.log(generateReceipt(receiptItems, totalPrice))
+    let itemInfos = getUnitPriceAndName(barcodes);
+    let itemList = integrateSameItems(itemInfos);
+    let receiptItems = calculateSubtotalPrice(itemList);
+    let totalPrice = calculateTotalPrice(receiptItems);
+    console.log(generateReceipt(receiptItems, totalPrice));
 }
 function getUnitPriceAndName(barcodes) {
-    let itemInfos = []
-    let itemDetailInfos = getItemDetailInfos()
+    let itemInfos = [];
+    let itemDetailInfos = getItemDetailInfos();
     for (let indexOfBarcodes = 0; indexOfBarcodes < barcodes.length; indexOfBarcodes++) {
         for (let indexOfItemDetailInfos = 0; indexOfItemDetailInfos < itemDetailInfos.length; indexOfItemDetailInfos++) {
             if (barcodes[indexOfBarcodes] === itemDetailInfos[indexOfItemDetailInfos].barcode) {
@@ -15,53 +15,54 @@ function getUnitPriceAndName(barcodes) {
                     barcode: barcodes[indexOfBarcodes], 
                     name: itemDetailInfos[indexOfItemDetailInfos].name,
                     unitPrice: itemDetailInfos[indexOfItemDetailInfos].price
-                })
-                break
+                });
+                break;
             }
         }
     }
-    return itemInfos
+    return itemInfos;
 }
 function integrateSameItems(itemInfos) {
-    let itemList = []
+    let itemList = [];
     for (let indexOfItemInfos = 0; indexOfItemInfos < itemInfos.length; indexOfItemInfos++) {
-        let hasSameItem = false
+        let hasSameItem = false;
         for (let indexOfItemList = 0; indexOfItemList < itemList.length; indexOfItemList++) {
             if (itemList[indexOfItemList].barcode === itemInfos[indexOfItemInfos].barcode) {
-                itemList[indexOfItemList].quantity++
-                hasSameItem = true
-                break
+                itemList[indexOfItemList].quantity++;
+                hasSameItem = true;
+                break;
             }
         }
         if (!hasSameItem) {
-            itemInfos[indexOfItemInfos].quantity = 1
-            itemList.push(itemInfos[indexOfItemInfos])
+            itemInfos[indexOfItemInfos].quantity = 1;
+            itemList.push(itemInfos[indexOfItemInfos]);
         }
     }
-    return itemList
+    return itemList;
 }
 function calculateSubtotalPrice(itemList) {
-    let receiptItems = itemList
+    let receiptItems = itemList;
     for (let indexOfReceiptItems = 0; indexOfReceiptItems < receiptItems.length; indexOfReceiptItems++) {
-        receiptItems[indexOfReceiptItems].subtotalPrice = receiptItems[indexOfReceiptItems].quantity * receiptItems[indexOfReceiptItems].unitPrice
+        receiptItems[indexOfReceiptItems].subtotalPrice = receiptItems[indexOfReceiptItems].quantity * receiptItems[indexOfReceiptItems].unitPrice;
     }
-    return receiptItems
+    return receiptItems;
 }
 function calculateTotalPrice(receiptItems) {
-    let totalPrice = 0
+    let totalPrice = 0;
     for (let indexOfReceiptItems = 0; indexOfReceiptItems < receiptItems.length; indexOfReceiptItems++) {
-        totalPrice += receiptItems[indexOfReceiptItems].subtotalPrice
+        totalPrice += receiptItems[indexOfReceiptItems].subtotalPrice;
     }
-    return totalPrice
+    return totalPrice;
 }
 function generateReceipt(receiptItems, totalPrice) {
-    let receipt = ''
-    receipt += '\n***<store earning no money>Receipt ***\n'
+    let receipt = '';
+    receipt += '\n***<store earning no money>Receipt ***\n';
     for (let indexOfReceiptItems = 0; indexOfReceiptItems < receiptItems.length; indexOfReceiptItems++) {
-        receipt += `Name: ${receiptItems[indexOfReceiptItems].name}, Quantity: ${receiptItems[indexOfReceiptItems].quantity}, Unit price: ${receiptItems[indexOfReceiptItems].unitPrice} (yuan), Subtotal: ${receiptItems[indexOfReceiptItems].subtotalPrice} (yuan)\n`
+        receipt += `Name: ${receiptItems[indexOfReceiptItems].name}, Quantity: ${receiptItems[indexOfReceiptItems].quantity}, ` + 
+        `Unit price: ${receiptItems[indexOfReceiptItems].unitPrice} (yuan), Subtotal: ${receiptItems[indexOfReceiptItems].subtotalPrice} (yuan)\n`;
     }
-    receipt += `----------------------\nTotal: ${totalPrice} (yuan)\n**********************`
-    return receipt
+    receipt += `----------------------\nTotal: ${totalPrice} (yuan)\n**********************`;
+    return receipt;
 }
 function getItemDetailInfos() {
     return [
@@ -95,7 +96,7 @@ function getItemDetailInfos() {
            name: 'Instant Noodles',
            price: 4
          }
-     ]
+     ];
 }
 
 module.exports = {
